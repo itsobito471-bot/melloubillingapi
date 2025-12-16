f2d8a8602f5c285f5c0bfdb686912d4290171904
ADIL <anwaradil295@gmail.com>
2025-12-16 16:40:42 +0530

    chore: Update default server port to 3000.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const morgan = require('morgan');
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
