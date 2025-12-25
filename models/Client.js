1883034f724565a64943edc3e23a06df7eea3d45
ADIL <anwaradil295@gmail.com>
2025-12-25 20:58:52 +0530

    feat: Add `resellerMargin` field to Client model.

--- models/Client.js
+++ models/Client.js
@@ -18,6 +18,10 @@ const ClientSchema = new mongoose.Schema({
         type: String,
         enum: ['Active', 'Inactive'],
         default: 'Active'
+    },
+    resellerMargin: {
+        type: Number,
+        default: 0
     }
 }, {
     timestamps: true
